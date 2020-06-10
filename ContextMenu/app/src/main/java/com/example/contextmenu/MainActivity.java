4bc429caca16b3237769334307fd882025abe0ef
Nallamilli S V S R Krushik Reddy <krushikreddynallamilli@gmail.com>
2020-06-10 12:12:52 +0530

    Activity life cycle

---
+++ ContextMenu/app/src/main/java/com/example/contextmenu/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.contextmenu;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.text_view);
+
+        registerForContextMenu(textView);
+
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        getMenuInflater().inflate(R.menu.example_menu,menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.option_1:
+                Toast.makeText(this, "option 1 is selected", Toast.LENGTH_SHORT).show();
+                break;
+            case (R.id.option_2):
+                Toast.makeText(this, "option 2 is selected", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onContextItemSelected(item);
+    }
+}
